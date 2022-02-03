2e25caa3065585d45c746181e4699e4a954967b7
Andrew N. Shalaev <isqad88@gmail.com>
2022-02-03 16:59:28 +0500

    User messages db schema

---
+++ db/migrate/20220202171102_create_user_messages.down.sql
@@ -0,0 +1,13 @@
+DROP TABLE user_messages_gifts;
+DROP TABLE user_messages_videos;
+DROP TABLE user_messages_videos_messages;
+
+DROP TABLE user_messages_images;
+DROP TABLE user_messages_videos_messages;
+DROP TABLE user_messages_voices;
+DROP TABLE user_messages;
+DROP TYPE user_messages_content_type;
+
+DROP TABLE user_dialogs;
+DROP TABLE dialogs;
+DROP TYPE dialogs_type;
---
+++ internal/sfu/dialogs.go
@@ -0,0 +1,5 @@
+package sfu
+
+type Dialog struct {
+	ID string `json:"id" db:"id"`
+}
