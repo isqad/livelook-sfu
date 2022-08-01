3376b9679e1e036e23b725b2a2fc5d04b576e2ce
Andrew N. Shalaev <isqad88@gmail.com>
2022-08-01 19:48:05 +0500

    feat: start and stop ffmpeg

--- internal/transcode/daemon.go
+++ internal/transcode/daemon.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
 	"sync"
 
 	"github.com/isqad/livelook-sfu/internal/core"
@@ -14,7 +16,7 @@ import (
 )
 
 type Daemon struct {
-	sync.Mutex
+	sync.RWMutex
 	nc                 *nats.Conn
 	startTranscoderSub *nats.Subscription
 	stopTranscoderSub  *nats.Subscription
@@ -32,9 +34,9 @@ func New(natsAddr string) (*Daemon, error) {
 	}
 
 	daemon := &Daemon{
-		nc:     nc,
-		errors: make(chan error),
-		stop:   make(chan struct{}),
+		nc:            nc,
+		errors:        make(chan error),
+		stop:          make(chan struct{}),
 		tanscoderPids: make(map[core.UserSessionID]int),
 	}
 
@@ -48,7 +50,7 @@ func (d *Daemon) Run() error {
 
 	// подписка на запуск транскодера, сообщение получает только один из запущенных сервисов
 	d.startTranscoderSub, err = d.nc.QueueSubscribe(TranscoderStartSubj, TranscoderQueueHLS, func(msg *nats.Msg) {
-		go handleStartTranscoderMessage(msg, d.errors)
+		go d.handleStartTranscoderMessage(msg, d.errors)
 	})
 	if err != nil {
 		d.Stop()
@@ -58,7 +60,7 @@ func (d *Daemon) Run() error {
 	// подписка на остановку транскодера, сообщение получают все сервисы, т.к. не известно, какой сервис запустил данный
 	// транскодер
 	d.stopTranscoderSub, err = d.nc.Subscribe(TranscoderStopSubj, func(msg *nats.Msg) {
-		go handleStopTranscoderMessage(msg, d.errors)
+		go d.handleStopTranscoderMessage(msg, d.errors)
 	})
 	if err != nil {
 		d.Stop()
@@ -87,12 +89,12 @@ func (d *Daemon) Stop() {
 		log.Error().Err(err).Msg("")
 	}
 
-	if err := d.nc.Drain(); err !=nil {
+	if err := d.nc.Drain(); err != nil {
 		log.Error().Err(err).Msg("")
 	}
 }
 
-func handleStartTranscoderMessage(msg *nats.Msg, errChan chan<- error) {
+func (d *Daemon) handleStartTranscoderMessage(msg *nats.Msg, errChan chan<- error) {
 	log.Debug().Str("data", string(msg.Data[:])).Msg("received message, try to start transcoder")
 
 	payload := &Message{}
@@ -104,22 +106,41 @@ func handleStartTranscoderMessage(msg *nats.Msg, errChan chan<- error) {
 		return
 	}
 
-
-	if err := startTranscoder(payload); err != nil {
+	if err := d.startTranscoder(payload); err != nil {
 		errChan <- err
 	}
 }
 
-func handleStopTranscoderMessage(msg *nats.Msg, errChan chan<- error) {
+func (d *Daemon) handleStopTranscoderMessage(msg *nats.Msg, errChan chan<- error) {
+	payload := &Message{}
+
+	r := bytes.NewReader(msg.Data)
+	err := json.NewDecoder(r).Decode(payload)
+	if err != nil {
+		errChan <- fmt.Errorf("transcode error: %v, payload: %s", err, string(msg.Data[:]))
+		return
+	}
+
+	if err := d.stopTranscoder(payload.UserID); err != nil {
+		errChan <- err
+	}
 }
 
-func startTranscoder(payload *Message) error {
+func (d *Daemon) startTranscoder(payload *Message) error {
 	rootDir := viper.GetString("app.streams_root_dir")
-	if err := os.MkdirAll(rootDir+"/"+string(payload.UserID), 0755); err != nil {
+	userDir := rootDir + "/" + string(payload.UserID)
+	streamDir := rootDir + "/" + string(payload.UserID) + "/stream"
+
+	if err := os.MkdirAll(userDir, 0755); err != nil {
+		return err
+	}
+	if err := os.MkdirAll(streamDir, 0755); err != nil {
 		return err
 	}
 
-	f, err := os.Create(rootDir + "/" + string(payload.UserID) + "/transcoder.sdp")
+	sdpFilePath := userDir + "/transcoder.sdp"
+
+	f, err := os.Create(sdpFilePath)
 	if err != nil {
 		return err
 	}
@@ -131,9 +152,94 @@ func startTranscoder(payload *Message) error {
 	}
 	f.Close()
 
+	m3u8FilePath := streamDir + "/stream.m3u8"
+
+	ffmpegCmd := exec.Command(
+		"ffmpeg",
+		"-protocol_whitelist", "file,udp,rtp",
+		"-i", sdpFilePath,
+		"-c:v", "libx264",
+		"-preset", "veryfast",
+		"-crf", "18",
+		"-b:v", "3000k",
+		"-maxrate", "3000k",
+		"-bufsize", "6000k",
+		"-pix_fmt", "yuv420p",
+		"-g", "30",
+		"-flags", "low_delay",
+		"-hls_time", "2",
+		"-hls_flags", "delete_segments",
+		"-hls_list_size", "5",
+		m3u8FilePath,
+	)
+
+	ffmpegCmdIn, err := ffmpegCmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+	ffmpegCmdIn.Close()
+
+	ffmpegCmdOut, err := ffmpegCmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		if _, err := io.Copy(os.Stdout, ffmpegCmdOut); err != nil {
+			log.Error().Err(err).Msg("close ffmpeg cmd out")
+		}
+	}()
+
+	err = ffmpegCmd.Start()
+	if err != nil {
+		return err
+	}
+
+	pid := ffmpegCmd.Process.Pid
+
+	log.Info().Int("pid", pid).Msg("ffmpeg started")
+
+	d.Lock()
+	d.tanscoderPids[payload.UserID] = pid
+	d.Unlock()
+
+	if err := ffmpegCmd.Wait(); err != nil {
+		log.Error().Err(err).Msg("ffmpeg error")
+	}
+
 	return nil
 }
 
-func stopTranscoder(userID core.UserSessionID) error {
+func (d *Daemon) stopTranscoder(userID core.UserSessionID) error {
+	var pid int
+
+	d.RLock()
+	pid, ok := d.tanscoderPids[userID]
+	if !ok {
+		d.RUnlock()
+		return nil
+	}
+	d.RUnlock()
+
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		return err
+	}
+	if err := process.Kill(); err != nil {
+		log.Error().Err(err).Msg("ffmpeg error")
+	}
+
+	d.Lock()
+	delete(d.tanscoderPids, userID)
+	defer d.Unlock()
+
+	log.Info().Int("pid", pid).Msg("ffmpeg stopped")
+
+	rootDir := viper.GetString("app.streams_root_dir")
+	userDir := rootDir + "/" + string(userID)
+	if err := os.RemoveAll(userDir); err != nil {
+		return err
+	}
+
 	return nil
 }
